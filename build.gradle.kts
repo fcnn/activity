bfc193ac7db153070b1614437f41a501356d01b6
y <y.com>
2022-06-09 12:25:40 +0800

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
   extra.apply {
     set("grpcVersion", "+")
     set("protobufVersion", "+")
-    set("kotlinVersion", "1.7.0-RC")
+    set("kotlinVersion", "1.7.0-RC2")
     set("protocVersion", "+")
     set("protobufPluginVersion", "0.8.18")
   }
