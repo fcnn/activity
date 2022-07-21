bc9d81a2e959c759142ac9d32633620f70e397ec
y <y@y.com>
2022-07-20 23:43:13 -0400

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ plugins {
   kotlin("jvm") version("${property("kotlinVersion")}")
   application
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("io.spring.dependency-management") version("1.0.11.RELEASE")
+  id("io.spring.dependency-management") version("1.0.12.RELEASE")
 }
 
 group = "com.hk"
